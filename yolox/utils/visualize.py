d2ed4c6e3bc9cbdbf20953c09bf799936a4eba17
Md-Ferdous <55968913+Md-Ferdous@users.noreply.github.com>
2021-08-12 19:10:47 +0600

    Update visualize.py

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -25,10 +25,13 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
         text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
         txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
         font = cv2.FONT_HERSHEY_SIMPLEX
-
+ 
         txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
         cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
-
+        text_f = '{} {:.1f} {} {} {} {}'.format(class_names[cls_id], score * 100, x0, y0, x1, y1)
+        file = open('det.txt', 'a')
+        file.write(text_f)
+        file.write("\n")
         txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
         cv2.rectangle(
             img,
