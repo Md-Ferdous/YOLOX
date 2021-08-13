661f4a3341b89e752fb086de150e79a1bb9c8903
Md-Ferdous <55968913+Md-Ferdous@users.noreply.github.com>
2021-08-13 23:33:43 +0600

    Update visualize.py

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -8,7 +8,7 @@ import numpy as np
 __all__ = ["vis"]
 
 
-def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
+def vis(p, img, boxes, scores, cls_ids, conf=0.5, class_names=None):
 
     for i in range(len(boxes)):
         box = boxes[i]
@@ -29,7 +29,7 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
         txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
         cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
         text_f = '{} {:.1f} {} {} {} {}'.format(class_names[cls_id], score * 100, x0, y0, x1, y1)
-        file = open('det.txt', 'a')
+        file = open(p, 'a')
         file.write(text_f)
         file.write("\n")
         txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
