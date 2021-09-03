34e454e543f885c162d1e0627a1f7f25e0ad1947
Md-Ferdous <55968913+Md-Ferdous@users.noreply.github.com>
2021-09-03 19:33:16 +0600

    Update checkpoint.py

--- yolox/utils/checkpoint.py
+++ yolox/utils/checkpoint.py
@@ -42,3 +42,18 @@ def save_checkpoint(state, is_best, save_dir, model_name=""):
     if is_best:
         best_filename = os.path.join(save_dir, "best_ckpt.pth.tar")
         shutil.copyfile(filename, best_filename)
+        
+        
+def save_checkpoint(state, is_best, save_dir, model_name=""):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    filename = os.path.join(save_dir, model_name + "_ckpt.pth")
+    filename1 = os.path.join('/content/drive/My Drive/covid waste res/yolox-save/', model_name + "_ckpt.pth")
+    torch.save(state, filename)
+    torch.save(state, filename1)
+    if is_best:
+        best_filename = os.path.join(save_dir, "best_ckpt.pth")
+        shutil.copyfile(filename, best_filename)
+        best_filename1 = os.path.join('/content/drive/My Drive/covid waste res/yolox-save/', "best_ckpt.pth")
+        shutil.copyfile(filename, best_filename1)
+        
