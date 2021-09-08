9278b2aefc69352f78df40179c246f7ad2d6ea5f
Md-Ferdous <55968913+Md-Ferdous@users.noreply.github.com>
2021-09-08 22:53:53 +0600

    Update checkpoint.py

--- yolox/utils/checkpoint.py
+++ yolox/utils/checkpoint.py
@@ -32,18 +32,7 @@ def load_ckpt(model, ckpt):
 
     model.load_state_dict(load_dict, strict=False)
     return model
-
-
-def save_checkpoint(state, is_best, save_dir, model_name=""):
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    filename = os.path.join(save_dir, model_name + "_ckpt.pth.tar")
-    torch.save(state, filename)
-    if is_best:
-        best_filename = os.path.join(save_dir, "best_ckpt.pth.tar")
-        shutil.copyfile(filename, best_filename)
-        
-        
+      
 def save_checkpoint(state, is_best, save_dir, model_name=""):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
