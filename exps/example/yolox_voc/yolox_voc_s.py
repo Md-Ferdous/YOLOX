b35e410b7d815b16d97de6017a0743c947e7c804
Md-Ferdous <55968913+Md-Ferdous@users.noreply.github.com>
2021-09-26 22:52:04 +0600

    Update yolox_voc_s.py

--- exps/example/yolox_voc/yolox_voc_s.py
+++ exps/example/yolox_voc/yolox_voc_s.py
@@ -12,8 +12,8 @@ class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
         self.num_classes = 20
-        self.depth = 0.33
-        self.width = 0.50
+        self.depth = 0.67
+        self.width = 0.75
         self.max_epoch = 100
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
