7a5b744e283d221075d35ca1f7a972e67221e055
Md-Ferdous <55968913+Md-Ferdous@users.noreply.github.com>
2022-04-10 04:33:02 +0600

    Update and rename yolox_voc_m.py to yolox_voc_l.py

--- exps/example/yolox_voc/yolox_voc_l.py
+++ exps/example/yolox_voc/yolox_voc_l.py
@@ -12,8 +12,8 @@ class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
         self.num_classes = 20
-        self.depth = 0.67
-        self.width = 0.75
+        self.depth = 1.0
+        self.width = 1.0
         self.max_epoch = 180
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
